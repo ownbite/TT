44368e4761b81007deb23737da0d1a33747bbb34
Hyper-Richard <brt2008@gmail.com>
2014-09-11 15:26:10 +0200

    MQs och mobilmenyer, mobilsök etc + diverse MQ tweaks

---
+++ wp-content/themes/Helsingborg/js/dev/hbg.dev.js
@@ -0,0 +1,26 @@
+/* HBG dev JS - To merged into app.js, minified with grunt */
+$(document).foundation({
+"magellan-expedition": {
+  active_class: 'active', // specify the class used for active sections
+  threshold: 0, // how many pixels until the magellan bar sticks, 0 = auto
+  destination_threshold: 20, // pixels from the top of destination for it to be considered active
+  throttle_delay: 50, // calculation throttling to increase framerate
+  fixed_top: 0, // top distance in pixels assigned to the fixed element on scroll
+}
+});
+
+
+$(document).ready(function(){
+
+  $('.show-support-nav').bind('click', function(){
+      $('.support-nav-list').toggle();
+  });
+
+  $('.show-mobile-nav').bind('click', function(){
+  		$('.mobile-nav-list').toggle();
+  });
+  $('.show-mobile-search').bind('click', function(){
+  		$('.mobile-search').toggle();
+  });
+
+});
