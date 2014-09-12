d292ffc1d57b83c38d6d592511b4d30efd659b1f
Hyper-Richard <brt2008@gmail.com>
2014-09-12 16:19:55 +0200

    artikelsida etc

--- wp-content/themes/Helsingborg/js/dev/hbg.dev.js
+++ wp-content/themes/Helsingborg/js/dev/hbg.dev.js
@@ -14,6 +14,7 @@ $(document).ready(function(){
 
   $('.show-support-nav').bind('click', function(){
       $('.support-nav-list').toggle();
+      $(this).toggleClass('active');
   });
 
   $('.show-mobile-nav').bind('click', function(){
