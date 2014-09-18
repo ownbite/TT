f6e92856a548a556cfe52217bdae63e2828c9005
Hyper-Richard <brt2008@gmail.com>
2014-09-18 16:16:32 +0200

    Listtabell

--- wp-content/themes/Helsingborg/js/dev/hbg.dev.js
+++ wp-content/themes/Helsingborg/js/dev/hbg.dev.js
@@ -1,16 +1,12 @@
 /* HBG dev JS - To merged into app.js, minified with grunt */
 $(document).foundation({
-"magellan-expedition": {
-  active_class: 'active', // specify the class used for active sections
-  threshold: 0, // how many pixels until the magellan bar sticks, 0 = auto
-  destination_threshold: 20, // pixels from the top of destination for it to be considered active
-  throttle_delay: 50, // calculation throttling to increase framerate
-  fixed_top: 0, // top distance in pixels assigned to the fixed element on scroll
-}
+
 });
 
 
 $(document).ready(function(){
+
+
 
   $('.show-support-nav').bind('click', function(){
       $('.support-nav-list').toggle();
@@ -26,4 +22,25 @@ $(document).ready(function(){
       $(this).toggleClass('active');
   });
 
+
+   if($('.table-list').length > 0) {
+     
+      $('.table-item').bind('click', function(){
+          if($(this).not('active')) {
+              $('.table-item').removeClass('active');
+              $('.table-content').removeClass('open');
+              $(this).addClass('active');
+              $(this).next('.table-content').addClass('open');
+          } else if($(this).hasClass('active')){
+              $('.table-item').removeClass('active');
+              $('.table-content').removeClass('open');
+          }
+      });
+
+      $('.table-list tr td:last-child').append('<span class="icon"></span>');
+      $('.table-list .table-item:odd').addClass('odd');
+      $('.table-list').tablesorter();
+    }
+
 });
+
