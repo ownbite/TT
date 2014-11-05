ca256573dfebb0fe0daee1d8b052e5aca9dfbd3a
Henric Lind <henric.lind@sogeti.se>
2014-11-05 16:40:21 +0100

    FIx: Updated so "* Nyhetsobjekt" now uses list-style instead of single objects. This due to all items need to be in sam unsorted list, and we don't want to add the <ul> outside the sidebar.

--- wp-content/plugins/helsingborg-widgets/classes/news-list-widget.php
+++ wp-content/plugins/helsingborg-widgets/classes/news-list-widget.php
@@ -23,86 +23,192 @@ if (!class_exists('News_List_Widget')) {
 }
 
 if (!class_exists('News_List_Widget_Box')) {
-{
   class News_List_Widget_Box extends WP_Widget {
 
     /** constructor */
     function News_List_Widget_Box() {
-      parent::WP_Widget(false, '* Nyhetsobjekt', array('description' => 'Renderar ut vald sida som nyhet.'));
+      parent::WP_Widget(false, '* Nyhetsobjekt', array('description' => 'Renderar ut valda sidor som nyheter.'));
     }
 
-    /* Front-end display of widget*/
-    /** @see WP_Widget::widget */
-    function widget($args, $instance) {
+    public function widget( $args, $instance ) {
       extract($args);
-      $page_id = (int) apply_filters('widget_title', $instance['page_id']);
 
-      if ( function_exists('icl_object_id') ) { $page_id = icl_object_id($page_id, "page"); }
+      // Get all the data saved
+      $amount = empty($instance['amount']) ? 1 : $instance['amount'];
 
-      if(!$page_id){
-        echo 'Ingen sida nyhetssida vald!';
-        return;
+      for ($i = 1; $i <= $amount; $i++) {
+        $items[$i-1] = $instance['item'.$i];
+        $item_ids[$i-1] = $instance['item_id'.$i];
+      } ?>
+
+      <section class="news-section">
+        <ul class="news-list-large row">
+        <?php // Go through all list items and present as a list
+        foreach ($items as $num => $item) :
+            $item_id = $item_ids[$num];
+            $page = get_page($item_id, OBJECT, 'display');
+
+            // Get the content, see if <!--more--> is inserted
+            $the_content = get_extended(strip_shortcodes($page->post_content));
+            $main = $the_content['main'];
+            $content = $the_content['extended']; // If content is empty, no <!--more--> tag was used -> content is in $main
+
+            $link = get_permalink($page->ID); ?>
+
+            <li class="news-item large-12 columns">
+              <div class="row">
+                <div class="large-4 medium-4 small-4 columns news-image">
+                <?php // Try to get the thumbnail for the page
+                if (has_post_thumbnail( $page->ID ) ) :
+                  $image_id = get_post_thumbnail_id( $page->ID );
+                  $image = wp_get_attachment_image_src( $image_id, 'single-post-thumbnail' );
+                  $alt_text = get_post_meta($image_id, '_wp_attachment_image_alt', true); ?>
+                  <img src="<?php echo $image[0]; ?>" alt="<?php echo $alt_text; ?>">
+                <?php endif; ?>
+                </div>
+
+                <div class="large-8 medium-8 small-8 columns news-content">
+                  <h2 class="news-title"><?php echo $page->post_title ?></h2>
+                  <span class="news-date"><?php echo $page->post_date ?></span>
+                  <?php echo wpautop($main, true); ?>
+                  <a href='<?php echo $link ?>' class="read-more">Läs mer</a>
+                </div>
+              </div>
+            </li>
+
+        <?php endforeach; ?>
+        </ul>
+      </section>
+
+      <?php
+    }
+
+    public function update( $new_instance, $old_instance) {
+
+      // Save the data
+      $amount = $new_instance['amount'];
+      $new_item = empty($new_instance['new_item']) ? false : strip_tags($new_instance['new_item']);
+
+      if ( isset($new_instance['position1'])) {
+        for($i=1; $i<= $new_instance['amount']; $i++){
+          if($new_instance['position'.$i] != -1){
+            $position[$i] = $new_instance['position'.$i];
+          }else{
+            $amount--;
+          }
+        }
+        if($position){
+          asort($position);
+          $order = array_keys($position);
+          if(strip_tags($new_instance['new_item'])){
+            $amount++;
+            array_push($order, $amount);
+          }
+        }
+
+      }else{
+        $order = explode(',',$new_instance['order']);
+        foreach($order as $key => $order_str){
+          $num = strrpos($order_str,'-');
+          if($num !== false){
+            $order[$key] = substr($order_str,$num+1);
+          }
+        }
       }
 
-      // Get the page and it's link
-      $page = get_page($page_id, OBJECT, 'display');
+      if($order){
+        foreach ($order as $i => $item_num) {
+          $instance['item'.($i+1)] = empty($new_instance['item'.$item_num]) ? '' : strip_tags($new_instance['item'.$item_num]);
+          $instance['item_id'.($i+1)] = empty($new_instance['item_id'.$item_num]) ? '' : strip_tags($new_instance['item_id'.$item_num]);
+        }
+      }
 
-      // Get the content, see if <!--more--> is inserted
-      $the_content = get_extended(strip_shortcodes($page->post_content));
-      $main = $the_content['main'];
-      $content = $the_content['extended']; // If content is empty, no <!--more--> tag was used -> content is in $main
+      $instance['amount'] = $amount;
 
-      $link = get_permalink($page->ID); ?>
-
-      <li class="news-item large-12 columns">
-        <div class="row">
-          <div class="large-4 medium-4 small-4 columns news-image">
-            <?php // Try to get the thumbnail for the page
-            if (has_post_thumbnail( $page->ID ) ) :
-              $image_id = get_post_thumbnail_id( $page->ID );
-              $image = wp_get_attachment_image_src( $image_id, 'single-post-thumbnail' );
-              $alt_text = get_post_meta($image_id, '_wp_attachment_image_alt', true);
-              ?>
-              <img src="<?php echo $image[0]; ?>" alt="<?php echo $alt_text; ?>">
-            <?php endif; ?>
-          </div>
-
-          <div class="large-8 medium-8 small-8 columns news-content">
-            <h2 class="news-title"><?php echo $page->post_title ?></h2>
-            <span class="news-date"><?php echo $page->post_date ?></span>
-            <?php echo wpautop($main, true); ?>
-            <a href='<?php echo $link ?>' class="read-more">Läs mer</a>
-          </div>
-        </div>
-      </li>
-
-    <?php }
-
-    /** @see WP_Widget::update */
-    function update($new_instance, $old_instance) {
-      $instance = $old_instance;
-      $instance['page_id'] = (int) $new_instance['page_id'];
       return $instance;
     }
 
-    /** @see WP_Widget::form */
-    function form($instance) {
-      $page_id = 0;
+    public function form ( $instance ) {
+      $amount = empty($instance['amount']) ? 1 : $instance['amount'];
 
-      if (isset($instance['page_id'])) {
-        $page_id = (int) esc_attr($instance['page_id']);
+      for ($i = 1; $i <= $amount; $i++) {
+        $items[$i] = empty($instance['item'.$i]) ? '' : $instance['item'.$i];
+        $item_ids[$i] = empty($instance['item_id'.$i]) ? '' : $instance['item_id'.$i];
       }
+  ?>
 
-      $pageIdArgs = array(
-        'selected' => $page_id,
-        'name' => $this->get_field_name('page_id'),
-      ); ?>
+      <ul class="hbgllw-instructions">
+        <li><?php echo __("Lägg till de sidor som ni vill ska visas i listan."); ?></li>
+      </ul>
 
-      <p><?php wp_dropdown_pages($pageIdArgs); ?></p>
+      <div class="helsingborg-link-list">
+      <?php foreach ($items as $num => $item) :
+        $item = esc_attr($item);
+        $item_id = esc_attr($item_ids[$num]);
+        $h5 = esc_attr($item);
+        if (!empty($item_id)) {
+          $h5 = get_post($item_id, OBJECT, 'display')->post_title;
+        }
+      ?>
 
-  <?php
+        <div id="<?php echo $this->get_field_id($num); ?>" class="list-item">
+          <h5 class="moving-handle"><span class="number"><?php echo $num; ?></span>. <span class="item-title"><?php echo $h5; ?></span><a class="hbgllw-action hide-if-no-js"></a></h5>
+          <div class="hbgllw-edit-item">
+            <p>
+              <label for="<?php echo $this->get_field_id('item_id'.$num); ?>"><?php echo __("Sida att hämta: "); ?></label><br>
+              <?php wp_dropdown_pages(array(
+                      'show_option_none' => 'Ingen sida vald',
+                      'selected' => $item_ids[$num],
+                      'id' => $this->get_field_id('item_id'.$num),
+                      'name' => $this->get_field_name('item_id'.$num)
+                    )); ?>
+            </p>
+            <a class="hbgllw-delete hide-if-no-js"><img src="<?php echo plugins_url('../images/delete.png', __FILE__ ); ?>" /> <?php echo __("Remove"); ?></a>
+            <br>
+          </div>
+        </div>
 
-      }
+      <?php endforeach;
+
+      if ( isset($_GET['editwidget']) && $_GET['editwidget'] ) : ?>
+        <table class='widefat'>
+          <thead><tr><th><?php echo __("Item"); ?></th><th><?php echo __("Position/Action"); ?></th></tr></thead>
+          <tbody>
+            <?php foreach ($items as $num => $item) : ?>
+            <tr>
+              <td><?php echo esc_attr($item); ?></td>
+              <td>
+                <select id="<?php echo $this->get_field_id('position'.$num); ?>" name="<?php echo $this->get_field_name('position'.$num); ?>">
+                  <option><?php echo __('&mdash; Select &mdash;'); ?></option>
+                  <?php for($i=1; $i<=count($items); $i++) {
+                    if($i==$num){
+                      echo "<option value='$i' selected>$i</option>";
+                    }else{
+                      echo "<option value='$i'>$i</option>";
+                    }
+                  } ?>
+                  <option value="-1"><?php echo __("Delete"); ?></option>
+                </select>
+              </td>
+            </tr>
+            <?php endforeach; ?>
+          </tbody>
+        </table>
+
+        <div class="hbgllw-row">
+          <input type="checkbox" name="<?php echo $this->get_field_name('new_item'); ?>" id="<?php echo $this->get_field_id('new_item'); ?>" /> <label for="<?php echo $this->get_field_id('new_item'); ?>"><?php echo __("Add New Item"); ?></label>
+        </div>
+      <?php endif; ?>
+
+      </div>
+      <div class="hbgllw-row hide-if-no-js">
+        <a class="hbgllw-add button-secondary"><img src="<?php echo plugins_url('../images/add.png', __FILE__ )?>" /> <?php echo __("Lägg till indexobjekt"); ?></a>
+      </div>
+
+      <input type="hidden" id="<?php echo $this->get_field_id('amount'); ?>" class="amount" name="<?php echo $this->get_field_name('amount'); ?>" value="<?php echo $amount ?>" />
+      <input type="hidden" id="<?php echo $this->get_field_id('order'); ?>" class="order" name="<?php echo $this->get_field_name('order'); ?>" value="<?php echo implode(',',range(1,$amount)); ?>" />
+
+<?php
     }
   }
 }
