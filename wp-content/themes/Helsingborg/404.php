06e1119588df4e69e8c83f32b384e37a5dfe0b93
henrikhelsingborg <henrik.wilhelmsson@helsingborg.se>
2014-11-19 14:59:31 +0100

    #1566 - 404-page. Not done! But needed to get latest version.

--- wp-content/themes/Helsingborg/404.php
+++ wp-content/themes/Helsingborg/404.php
@@ -1,25 +1,35 @@
 <?php get_header(); ?>
 
 <div class="row">
-	<div class="small-12 large-8 columns" role="main">
-
+	<div class="404">
 		<article <?php post_class() ?> id="post-<?php the_ID(); ?>">
 			<header>
-				<h1 class="entry-title"><?php _e('File Not Found', 'Helsingborg'); ?></h1>
+				<h1 class="entry-title"><?php _e('Sidan kan inte hittas', 'Helsingborg'); ?></h1>
 			</header>
+			<div class="small-12 large-12 columns" role="main">
 			<div class="entry-content">
+				<div class="search-container row">
+						<div class="search-inputs large-12 columns">
+								<form action="/TT/sok" method="GET"> <!-- OBS! SÖKVÄGEN -->
+									<input type="text" placeholder="Vad letar du efter?" name="q"/>
+									<input type="submit" value="Sök">
+								</form>
+						</div>
+				</div><!-- /.search-container -->
 				<div class="error">
-					<p class="bottom"><?php _e('The page you are looking for might have been removed, had its name changed, or is temporarily unavailable.', 'Helsingborg'); ?></p>
+					<p class="bottom"><?php _e('Den sida du vill nå kan inte hittas. Du kan använda sökrutan här ovanför för att försöka hitta det du letar efter.', 'Helsingborg'); ?></p>
 				</div>
-				<p><?php _e('Please try the following:', 'Helsingborg'); ?></p>
+				<p><?php _e('<h2>Det här kan du göra för att hitta det du söker:</h2>', 'Helsingborg'); ?></p>
 				<ul>
-					<li><?php _e('Check your spelling', 'Helsingborg'); ?></li>
-					<li><?php printf(__('Return to the <a href="%s">home page</a>', 'Helsingborg'), home_url()); ?></li>
-					<li><?php _e('Click the <a href="javascript:history.back()">Back</a> button', 'Helsingborg'); ?></li>
+					<li><?php _e('Kontrollera stavningen. Har du skrivit rätt adress?', 'Helsingborg'); ?></li>
+					<li><?php _e('Använd sökrutan ovanför för att söka rätt på det du letar efter.', 'Helsingborg'); ?></li>
+					<li><?php printf(__('Gå till <a href="%s">startsidan</a>', 'Helsingborg'), home_url()); ?></li>
+					<li><?php _e('Använd <a href="javascript:history.back()">webbläsarens bakåt-knapp</a>', 'Helsingborg'); ?></li>
 				</ul>
+				<p><?php _e('<h2>Vill du komma i kontakt med Helsingborgs stad?</h2> Du kan ringa till Helsingborg kontaktcenter på telefonnummer 042-10 50 00 för att bli lotsad rätt.', 'Helsingborg'); ?></p>
 			</div>
+		</div>
 		</article>
-
 	</div>
 	<?php get_sidebar(); ?>
 </div>
