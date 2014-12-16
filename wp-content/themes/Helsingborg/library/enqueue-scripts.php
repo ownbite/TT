3f15524cb0d211f2b2c5376e05e7ef8684ac3596
Henric Lind <henric.lind@sogeti.se>
2014-12-16 13:48:20 +0100

    #1548 - Added custom CSS for WP Page Widgets which maximizes the width for the area.

--- wp-content/themes/Helsingborg/library/enqueue-scripts.php
+++ wp-content/themes/Helsingborg/library/enqueue-scripts.php
@@ -44,6 +44,13 @@ if (!function_exists('Helsingborg_scripts')) :
   }
 
   add_action( 'wp_enqueue_scripts', 'Helsingborg_scripts' );
+
+  function load_custom_wp_admin_style() {
+    wp_register_style( 'custom_wp_admin_css', get_template_directory_uri() . '/css/admin-hbg.css', false, '1.0.0' );
+    wp_enqueue_style( 'custom_wp_admin_css' );
+  }
+  add_action( 'admin_enqueue_scripts', 'load_custom_wp_admin_style' );
+
 endif;
 
 ?>
