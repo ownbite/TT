3b6b329972c1ef9a558613819db3982aab5385e5
Henric Lind <henric.lind@sogeti.se>
2015-03-04 15:48:55 +0100

    #1667 - Guide now works with nested list items as well. Also fixed issue with pager not working for 2-digit values.

--- wp-content/plugins/helsingborg-widgets/js/steps.js
+++ wp-content/plugins/helsingborg-widgets/js/steps.js
@@ -1,70 +1,72 @@
 jQuery(document).ready(function($) {
 
-  if ($('.guide-list li.current:first-child').length) {
-    $('.prev-step').hide();
-  }
+	if ($('.guide-list li.current:first-child').length) {
+		$('.prev-step').hide();
+	}
 
-  $('.next-step').on('click', function(e) {
-    e.preventDefault();
+	$('.next-step').on('click', function(e) {
+		e.preventDefault();
 
-    var currentElem = $('.guide-list li.current');
+		var currentElem = $('.guide-list li.current');
 
-    currentElem.next().addClass("current");
-    currentElem.removeClass("current");
-    removeNext();
-    removePrev();
-    setPager();
-  });
+		currentElem.next().addClass("current");
+		currentElem.removeClass("current");
+		removeNext();
+		removePrev();
+		setPager();
+	});
 
-  $('.prev-step').on('click', function(e) {
-    e.preventDefault();
+	$('.prev-step').on('click', function(e) {
+		e.preventDefault();
 
-    var currentElem = $('.guide-list li.current');
+		var currentElem = $('.guide-list li.current');
 
-    currentElem.prev().addClass("current");
-    currentElem.removeClass("current");
-    removePrev();
-    removeNext();
-    setPager();
-  });
+		currentElem.prev().addClass("current");
+		currentElem.removeClass("current");
+		removePrev();
+		removeNext();
+		setPager();
+	});
 
-  $('.pagination li').on('click', function(e) {
-    if (this.innerText.length <= 2 && this.innerText.length > 0) {
-      e.preventDefault();
+	$('.pagination li').on('click', function(e) {
+		if (this.innerText.length <= 3 && this.innerText.length > 0) {
+			e.preventDefault();
 
-      var currentElem = $('.guide-list li.current');
-      var newElem = $('.guide-list li').eq(parseInt(this.innerText) - 1);
+			var currentElem = $('.guide-list li.current');
+			var newElem = $('.guide-list > li').not('.guide-list li ul li').eq(
+				parseInt(this.innerText) - 1);
 
-      currentElem.removeClass("current");
-      newElem.addClass('current');
-      removePrev();
-      removeNext();
-      setPager();
-    }
-  });
+			currentElem.removeClass("current");
+			newElem.addClass("current");
+			removePrev();
+			removeNext();
+			setPager();
+		}
+	});
 
-  function setPager() {
-    var newIndex = $('.guide-list li').index($('.guide-list li.current'));
+	function setPager() {
+		var newIndex = $('.guide-list > li').not('.guide-list li ul li').index($(
+			'.guide-list li.current'));
 
-    $('.pagination li.current-pager').removeClass('current-pager');
-    $('.pagination li').eq(newIndex + 1).addClass('current-pager');
-  }
+		$('.pagination li.current-pager').removeClass('current-pager');
+		$('.pagination li').eq(newIndex + 1).addClass('current-pager');
+	}
 
-  function removeNext() {
-    if ($('.guide-list li.current:last-child').length) {
-      $('.next-step').hide();
-    } else {
-      $('.next-step').show();
-    }
+	function removeNext() {
+		if ($('.guide-list li.current:last-child').length) {
+			$('.next-step').hide();
+		} else {
+			$('.next-step').show();
+		}
 
-  }
+	}
 
-  function removePrev() {
-    if ($('.guide-list li.current:first-child').length) {
-      $('.prev-step').hide();
-    } else {
-      $('.prev-step').show();
-    }
-  }
+	function removePrev() {
+		if ($('.guide-list li.current:first-child').length) {
+			$('.prev-step').hide();
+		} else {
+			$('.prev-step').show();
+		}
+	}
 
 });
