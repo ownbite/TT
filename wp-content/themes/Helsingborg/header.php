2a20ff1e433e20e49bfeb0854647c0b03d8ff019
Henric Lind <henric.lind@sogeti.se>
2015-03-24 09:58:42 +0100

    Added clear for browser cache in header.

--- wp-content/themes/Helsingborg/header.php
+++ wp-content/themes/Helsingborg/header.php
@@ -1,3 +1,9 @@
+<?php
+	header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
+	header('Cache-Control: no-store, no-cache, must-revalidate');
+	header('Cache-Control: post-check=0, pre-check=0', FALSE);
+	header('Pragma: no-cache');
+?>
 <!doctype html>
 <html class="no-js" <?php language_attributes(); ?> >
 	<head>
