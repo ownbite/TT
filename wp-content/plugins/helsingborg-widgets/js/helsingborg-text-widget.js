644b01c130c563657d593007c6f27f9a3ae81466
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2015-04-15 10:43:26 +0200

    #1749 - More fixes for the wysiwyg widget (hbg text widget)

--- wp-content/plugins/helsingborg-widgets/js/helsingborg-text-widget.js
+++ wp-content/plugins/helsingborg-widgets/js/helsingborg-text-widget.js
@@ -1,14 +1,51 @@
-jQuery('.widget-control-save').on('click', function () {
+/**
+ * Scroll to saved widget after page reload
+ */
+jQuery(document).ready(function ($) {
+    // Read the hash from url
+    var hash = location.hash.replace('#', '').split('|');
+    var sidebar = hash[1];
+    var widget = hash[0];
+
+    // Open and scroll to the widget (found in the hash)
+    $('#' + sidebar).parents('.widgets-holder-wrap').addClass('open').removeClass('closed');
+    $('[id*=_' + widget + '] .widget-inside').show();
+    var scrollTo = $('[id*=_' + widget + ']').offset().top;
+    $('html, body').animate({
+        scrollTop: scrollTo
+    }, 1000);
+});
+
+/**
+ * Save widget fix
+ */
+jQuery(document).on('click', '.widget-control-save', function () {
+    // Find tinymce textarea id
     var saveId = jQuery(this).attr('id');
     var id = saveId.replace( /-savewidget/, '' );
     var randNum = jQuery('#' + id + '-rand').val();
     var textareaId = id + '-hbgtexteditor_' + randNum;
 
+    // Trigger tinymce save on widget save
     tinyMCE.triggerSave();
-
-    setTimeout(function () {
-        location.reload();
-    }, 2000);
 });
 
-// widget-hbgtextwidget-5-hbgtexteditor_176
+/**
+ * Redirect/reload page after widget ajax update complete
+ */
+jQuery(document).ajaxSuccess(function (e, xhr, settings) {
+    var widgetBaseId = 'hbgtextwidget';
+
+    if(settings.data && settings.data.search('action=pw-save-widget') != -1 && settings.data.search('id_base=' + widgetBaseId) != -1) {
+        var setArr = decodeURIComponent(settings.data).split('&');
+        var parameters = new Array();
+
+        for (var i = setArr.length - 1; i >= 0; i--) {
+            var keyval = setArr[i].split('=');
+            parameters[keyval[0]] = keyval[1];
+        };
+
+        window.location.href = window.location.href.substr(0, window.location.href.indexOf('#')) + '#' + parameters['widget-id'] + '|' + parameters['sidebar'];
+        location.reload();
+    }
+});
