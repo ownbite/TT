9cf759310c4ff4ba5620e2984b0d26bd090b1b80
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2015-04-20 11:32:15 +0200

    #1763 - Youtube embed now appends when clicking a youtube gallery node

--- wp-content/plugins/helsingborg-gallery/views/gallery-template.php
+++ wp-content/plugins/helsingborg-gallery/views/gallery-template.php
@@ -1,29 +1,16 @@
 <div class="hbg-gallery-container">
     <?php foreach ($galleryItems as $item) : $item = (object) $item; ?>
     <div class="large-3 medium-4 small-6 columns">
-        <div class="hbg-gallery-item hbg-gallery-item-<?php echo $item->media; ?>">
+        <a href="#" class="hbg-gallery-item hbg-gallery-item-<?php echo $item->media; ?>" <?php if ($item->media == 'youtube') : ?>data-youtube="<?php echo $item->url; ?>"<?php endif; ?>>
             <div class="hbg-gallery-item-image-container" style="background-image:url('<?php echo $item->image_url?>');">
                 <img src="<?php echo $item->image_url?>" alt="<?php echo $item->title; ?>" class="hbg-gallery-item-image">
+                <?php if ($item->media == 'youtube') : ?>
+                <span class="youtube-play dashicons dashicons-video-alt3"></span>
+                <?php endif; ?>
             </div>
             <div class="hbg-gallery-item-title"><?php echo $item->title; ?></div>
             <div class="hbg-gallery-item-description"><?php echo $item->description; ?></div>
-
-            <?php
-                if ($item->media == 'youtube') {
-                    $matches = null;
-                    $rx = '~
-                        ^(?:https?://)?              # Optional protocol
-                         (?:www\.)?                  # Optional subdomain
-                         (?:youtube\.com|youtu\.be)  # Mandatory domain name
-                         /watch\?v=([^&]+)           # URI with video id as capture group 1
-                         ~x';
-
-                    preg_match($rx, $item->url, $matches);
-
-                    //echo '<iframe src="https://www.youtube.com/embed/' . $matches[1] . '?autoplay=1" frameborder="0" allowfullscreen></iframe>';
-                }
-            ?>
-        </div>
+        </a>
     </div>
     <?php endforeach; ?>
     <div class="clearfix"></div>
