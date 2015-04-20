dc359bdef83d520213bf37cbb0a3c8175aba70f7
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2015-04-20 16:27:02 +0200

    #1763 - Added jquery masinry instead of isotope.

--- wp-content/plugins/helsingborg-gallery/views/gallery-template.php
+++ wp-content/plugins/helsingborg-gallery/views/gallery-template.php
@@ -1,6 +1,5 @@
 <div class="hbg-gallery-container">
     <?php foreach ($galleryItems as $item) : $item = (object) $item; ?>
-    <div class="large-3 medium-4 small-6 columns">
         <a href="#" class="hbg-gallery-item hbg-gallery-item-<?php echo $item->media; ?>" <?php if ($item->media == 'youtube') : ?>data-youtube="<?php echo $item->url; ?>"<?php endif; ?>>
             <div class="hbg-gallery-item-image-container" style="background-image:url('<?php echo $item->image_url?>');">
                 <img src="<?php echo $item->image_url?>" alt="<?php echo $item->title; ?>" class="hbg-gallery-item-image">
@@ -11,7 +10,6 @@
             <div class="hbg-gallery-item-title"><?php echo $item->title; ?></div>
             <div class="hbg-gallery-item-description"><?php echo $item->description; ?></div>
         </a>
-    </div>
     <?php endforeach; ?>
     <div class="clearfix"></div>
 </div>
