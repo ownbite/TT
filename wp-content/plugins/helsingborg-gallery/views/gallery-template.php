722fd6324eb64f4c89dafc7173a3df36bfa06fab
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2015-04-21 10:06:50 +0200

    #1763 - Gallery front end functions and style

--- wp-content/plugins/helsingborg-gallery/views/gallery-template.php
+++ wp-content/plugins/helsingborg-gallery/views/gallery-template.php
@@ -4,11 +4,16 @@
             <div class="hbg-gallery-item-image-container" style="background-image:url('<?php echo $item->image_url?>');">
                 <img src="<?php echo $item->image_url?>" alt="<?php echo $item->title; ?>" class="hbg-gallery-item-image">
                 <?php if ($item->media == 'youtube') : ?>
-                <span class="youtube-play dashicons dashicons-video-alt3"></span>
+                <span class="hbg-gallery-media-icon dashicons dashicons-video-alt3"></span>
+                <?php elseif ($item->media == 'image') : ?>
+                <span class="hbg-gallery-media-icon dashicons dashicons-format-image"></span>
                 <?php endif; ?>
             </div>
             <div class="hbg-gallery-item-title"><?php echo $item->title; ?></div>
+
+            <?php if (isset($item->description) && strlen($item->description) > 0) : ?>
             <div class="hbg-gallery-item-description"><?php echo $item->description; ?></div>
+            <?php endif; ?>
         </a>
     <?php endforeach; ?>
     <div class="clearfix"></div>
