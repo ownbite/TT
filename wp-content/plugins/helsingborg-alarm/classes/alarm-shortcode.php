14e6e9b762375343f558032f352fad79b63c34f9
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2015-05-07 13:26:32 +0200

    #1797 - Alarm shortcode javascript enqueue optimization

--- wp-content/plugins/helsingborg-alarm/classes/alarm-shortcode.php
+++ wp-content/plugins/helsingborg-alarm/classes/alarm-shortcode.php
@@ -1,14 +1,17 @@
 <?php
 
-add_action( 'wp_enqueue_scripts', function(){
-  wp_enqueue_style( 'alarm-map-css', HELSINGBORG_ALARM_BASE . '/css/alarm-map.css' );
-  wp_enqueue_script( 'gmap-api-js', '//maps.googleapis.com/maps/api/js?key=AIzaSyDUKb32DWSPtPx5RN7BBd-gIuHPn8HpOBo', array(), '1.0.0', false );
-  wp_enqueue_script( 'alarm-widget-js', HELSINGBORG_ALARM_BASE . '/js/alarm-widget.js', array(), '1.0.0', false);
-  wp_localize_script( 'alarm-widget-js', 'ajaxalarm', array( 'url' => admin_url( 'admin-ajax.php' ) ) );
-});
+add_action( 'wp_enqueue_scripts', 'hbgAlarmShortcodeScripts');
+
+function hbgAlarmShortcodeScripts() {
+    wp_enqueue_script('alarm-widget-js', HELSINGBORG_ALARM_BASE . '/js/alarm-widget.js', array(), '1.0.0', false);
+    wp_localize_script('alarm-widget-js', 'ajaxalarm', array('url' => admin_url('admin-ajax.php')));
+}
 
 add_shortcode('alarm-map', function() {
-  return '<div id="map-canvas"></div>';
+    wp_enqueue_style('alarm-map-css', HELSINGBORG_ALARM_BASE . '/css/alarm-map.css');
+    wp_enqueue_script('gmap-api-js', '//maps.googleapis.com/maps/api/js?key=AIzaSyDUKb32DWSPtPx5RN7BBd-gIuHPn8HpOBo', array(), '1.0.0', false);
+
+    return '<div id="map-canvas"></div>';
 });
 
 ?>
