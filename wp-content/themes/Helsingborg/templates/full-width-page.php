2307fa887724594943bd009b634fb018b21301a5
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2015-05-25 09:19:32 +0200

    #1755 - LinkedIn share button and refactored share buttons to its own template file

--- wp-content/themes/Helsingborg/templates/full-width-page.php
+++ wp-content/themes/Helsingborg/templates/full-width-page.php
@@ -45,10 +45,7 @@ $content = $the_content['extended']; // If content is empty, no <!--more--> tag 
                     </div>
 
                     <footer>
-                        <ul class="socialmedia-list">
-                            <li class="fbook"><a href="https://www.facebook.com/sharer/sharer.php?u=<?php echo urlencode(get_the_permalink()); ?>">Facebook</a></li>
-                            <li class="twitter"><a href="http://twitter.com/share?url=<?php echo urlencode(wp_get_shortlink()); ?>">Twitter</a></li>
-                        </ul>
+                        <?php get_template_part('templates/partials/share'); ?>
                     </footer>
                 </article>
                 <?php endwhile; // End the loop ?>
