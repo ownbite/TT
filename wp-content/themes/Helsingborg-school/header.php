04d22db09ccfc787f52838e4466a58b05cd9ff84
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2015-05-26 14:14:39 +0200

    Adjusted the orbit slider style

--- wp-content/themes/Helsingborg-school/header.php
+++ wp-content/themes/Helsingborg-school/header.php
@@ -67,29 +67,41 @@
                     </div>
 
                     <div class="hero">
-                        <div class="columns large-12">
+                        <div class="columns large-12 no-margin">
                             <ul class="hero-orbit" data-orbit data-options="animation:fade; timer_speed:10000; slide_number:false;">
                                 <li style="background-image:url('http://www.gymnasieguiden.se/files/attachment/file/1526/DCC_vasteras3.jpg');">
                                     <div class="caption">
                                         <div class="caption-content">
-                                            Våra elever trivs i skolan.
-                                            <a class="read-more">Läs undersökningen här</a>
+                                            <div class="row">
+                                                <div class="columns large-12">
+                                                    Våra elever trivs i skolan.
+                                                    <a class="read-more">Läs undersökningen här</a>
+                                                </div>
+                                            </div>
                                         </div>
                                     </div>
                                 </li>
                                 <li style="background-image:url('http://www.procivitas.se/wp-content/uploads/2014/10/Skolan_ProCivitas.jpg');">
                                     <div class="caption">
                                         <div class="caption-content">
-                                            Maria och Petra berättar om varför de sökte till Tycho
-                                            <a class="read-more">Läs mer om hur du söker</a>
+                                            <div class="row">
+                                                <div class="columns large-12">
+                                                    Maria och Petra berättar om varför de sökte till Tycho
+                                                    <a class="read-more">Läs mer om hur du söker</a>
+                                                </div>
+                                            </div>
                                         </div>
                                     </div>
                                 </li>
                                 <li style="background-image:url('http://www.hd.se/Images/hdarchive/archive/01016/ST_Bal_Angelholm_2_1016216a.jpg');">
                                     <div class="caption">
                                         <div class="caption-content">
-                                            Treornas bal - en dag att minnas
-                                            <a class="read-more">Se bilderna från kvällen</a>
+                                            <div class="row">
+                                                <div class="columns large-12">
+                                                    Treornas bal - en dag att minnas
+                                                    <a class="read-more">Se bilderna från kvällen</a>
+                                                </div>
+                                            </div>
                                         </div>
                                     </div>
                                 </li>
