e9d0edff81b527f0dfe12f920e2daa1a0adc2b7a
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2015-05-26 12:05:22 +0200

    Added orbit slider to the hero section

--- wp-content/plugins/helsingborg-alarm/classes/alarm-shortcode.php
+++ wp-content/plugins/helsingborg-alarm/classes/alarm-shortcode.php
@@ -3,7 +3,7 @@
 add_action( 'wp_enqueue_scripts', 'hbgAlarmShortcodeScripts');
 
 function hbgAlarmShortcodeScripts() {
-    wp_enqueue_script('alarm-widget-js', HELSINGBORG_ALARM_BASE . '/js/alarm-widget.js', array(), '1.0.0', false);
+    wp_enqueue_script('alarm-widget-js', HELSINGBORG_ALARM_BASE . '/js/alarm-widget.js', array('jquery'), '1.0.0', false);
     wp_localize_script('alarm-widget-js', 'ajaxalarm', array('url' => admin_url('admin-ajax.php')));
 }
 
--- wp-content/themes/Helsingborg-school/header.php
+++ wp-content/themes/Helsingborg-school/header.php
@@ -67,7 +67,33 @@
                     </div>
 
                     <div class="hero">
-                        <div class="columns large-9" style="background-image:url('http://i.ytimg.com/vi/le_ccWDoAs8/maxresdefault.jpg');"></div>
-                        <div class="columns large-3"></div>
+                        <div class="columns large-12">
+                            <ul class="hero-orbit" data-orbit data-options="animation:fade; timer_speed:10000; slide_number:false;">
+                                <li style="background-image:url('http://www.gymnasieguiden.se/files/attachment/file/1526/DCC_vasteras3.jpg');">
+                                    <div class="caption">
+                                        <div class="caption-content">
+                                            Våra elever trivs i skolan.
+                                            <a class="read-more">Läs undersökningen här</a>
+                                        </div>
+                                    </div>
+                                </li>
+                                <li style="background-image:url('http://www.procivitas.se/wp-content/uploads/2014/10/Skolan_ProCivitas.jpg');">
+                                    <div class="caption">
+                                        <div class="caption-content">
+                                            Maria och Petra berättar om varför de sökte till Tycho
+                                            <a class="read-more">Läs mer om hur du söker</a>
+                                        </div>
+                                    </div>
+                                </li>
+                                <li style="background-image:url('http://www.hd.se/Images/hdarchive/archive/01016/ST_Bal_Angelholm_2_1016216a.jpg');">
+                                    <div class="caption">
+                                        <div class="caption-content">
+                                            Treornas bal - en dag att minnas
+                                            <a class="read-more">Se bilderna från kvällen</a>
+                                        </div>
+                                    </div>
+                                </li>
+                            </ul>
+                        </div>
                     </div>
                 </header>
