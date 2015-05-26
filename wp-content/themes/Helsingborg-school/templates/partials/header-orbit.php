94290bdb62b658600b8c7f3fc2c2f4b8cd42f052
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2015-05-26 14:49:02 +0200

    Added black welcome text area and made some other adjustments to header area

---
+++ wp-content/themes/Helsingborg-school/templates/partials/header-orbit.php
@@ -0,0 +1,42 @@
+<div class="hero">
+    <div class="columns large-12 no-margin">
+        <ul class="hero-orbit" data-orbit data-options="animation:fade; timer_speed:10000; slide_number:false;">
+            <li style="background-image:url('http://www.gymnasieguiden.se/files/attachment/file/1526/DCC_vasteras3.jpg');">
+                <div class="caption">
+                    <div class="caption-content">
+                        <div class="row">
+                            <div class="columns large-12">
+                                Våra elever trivs i skolan.
+                                <a class="read-more">Läs undersökningen här</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </li>
+            <li style="background-image:url('http://www.procivitas.se/wp-content/uploads/2014/10/Skolan_ProCivitas.jpg');">
+                <div class="caption">
+                    <div class="caption-content">
+                        <div class="row">
+                            <div class="columns large-12">
+                                Maria och Petra berättar om varför de sökte till Tycho
+                                <a class="read-more">Läs mer om hur du söker</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </li>
+            <li style="background-image:url('http://www.hd.se/Images/hdarchive/archive/01016/ST_Bal_Angelholm_2_1016216a.jpg');">
+                <div class="caption">
+                    <div class="caption-content">
+                        <div class="row">
+                            <div class="columns large-12">
+                                Treornas bal - en dag att minnas
+                                <a class="read-more">Se bilderna från kvällen</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </li>
+        </ul>
+    </div>
+</div>
---
+++ wp-content/themes/Helsingborg-school/templates/partials/header-welcome.php
@@ -0,0 +1,13 @@
+<div class="header-welcome">
+    <div class="row">
+        <div class="columns large-8">
+            <article>
+                <h1>Välkommen till din skola</h1>
+                <p>
+                    Hos oss är alla som är nyfikna på våra ämnen välkomna. Och du, du behöver inte vara en stjärna när du börjar här,  vi ser till att du blir en.
+                </p>
+                <p>Tycker du att det låter kaxigt? Vi håller alltid vad vi lovar.</p>
+            </article>
+        </div>
+    </div>
+</div>
