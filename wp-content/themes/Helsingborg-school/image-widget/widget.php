1373b11a62c1a4252b2e78b59fada9469756de33
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2015-05-28 12:54:24 +0200

    Widget style for image widget

--- wp-content/themes/Helsingborg-school/image-widget/widget.php
+++ wp-content/themes/Helsingborg-school/image-widget/widget.php
@@ -2,18 +2,32 @@
     if (!defined('ABSPATH')) die('-1');
 
     echo $before_widget;
-    echo $this->get_image_html($instance, true);
+
+    preg_match_all('#<a([^>])+>(.*?)</a>#i', $instance['description'], $matches);
+    $description = $instance['description'];
+
+    if (isset($matches[2][0])) {
+        $description = $matches[2][0];
+    }
 
 ?>
+    <?php if (!empty($instance['link'])) : ?>
+    <a href="<?php echo $instance['link']; ?>" target="<?php echo $instance['linktarget']; ?>">
+    <?php endif; ?>
 
-    <div class="widget-content-holder">
-        <?php echo (!empty($title)) ? $before_title . $title . $after_title : ''; ?>
+        <img src="<?php echo $instance['imageurl']; ?>" alt="<?php echo $instance['alt']; ?>">
+        <div class="widget-content-holder">
+            <?php echo (!empty($title)) ? $before_title . $title . $after_title : ''; ?>
 
-        <?php if (!empty($description)) : ?>
-        <div class="<?php echo $this->widget_options['classname']; ?>-description">
-            <?php echo wpautop($description); ?>
+            <?php if (!empty($description)) : ?>
+            <div class="<?php echo $this->widget_options['classname']; ?>-description">
+                <?php echo wpautop($description); ?>
+            </div>
+            <?php endif; ?>
         </div>
-        <?php endif; ?>
-    </div>
+
+    <?php if (!empty($instance['link'])) : ?>
+    </a>
+    <?php endif; ?>
 
 <?php echo $after_widget; ?>
